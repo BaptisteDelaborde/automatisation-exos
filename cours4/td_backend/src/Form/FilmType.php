f99cafbdfb8525554b5794f2e7f8b6c2ea003c1b
DioAkaTheWorld <burak.ozen7@etu.univ-lorraine.fr>
2026-01-26 16:04:05 +0100

    ajout des contrôleurs

---
+++ cours4/td_backend/src/Form/FilmType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Film;
+use App\Entity\Realisateur;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class FilmType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('titre')
+            ->add('duree')
+            ->add('annee')
+            ->add('realisateur', EntityType::class, [
+                'class' => Realisateur::class,
+                'choice_label' => 'id',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Film::class,
+        ]);
+    }
+}
